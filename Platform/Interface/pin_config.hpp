23f480859b20afb85194868b1e2db69ec487ad63
it32bit <it.wiejska@gmail.com>
2025-10-14 13:52:39 +0200

    Preparing GPIO for platform abstraction, step -2

--- Platform/Interface/pin_config.hpp
+++ Platform/Interface/pin_config.hpp
@@ -30,7 +30,6 @@
 
 #include <cstdint>
 #include <string_view>
-#include <array>
 
 struct PinConfig
 {
@@ -43,18 +42,58 @@ struct PinConfig
     };
     enum class Pull
     {
+        PullNone,
+        PullUp,
+        PullDown
+    };
+    enum class Type
+    {
+        PushPull,
+        OpenDrain
+    };
+    enum class Speed
+    {
+        Low,
+        Medium,
+        High,
+        VeryHigh
+    };
+    enum class InterruptTrigger
+    {
         None,
-        Up,
-        Down
+        Rising,
+        Falling,
+        RisingFalling
+    };
+
+    enum PortIndex : uint8_t
+    {
+        PortA = 0, // e.g., for STM32 and similar MCUs
+        PortB = 1,
+        PortC = 2,
+        PortD = 3,
+        PortE = 4,
+        PortF = 5,
+        PortG = 6,
+        PortH = 7,
+        PortI = 8,
+        Port0 = 0, // for ESP32 and similar MCUs
+        Port1 = 1,
+        Port2 = 2,
+        Port3 = 3,
+        Port4 = 4
     };
 
     std::string_view name;
-    uint8_t          port;              // e.g., 0 for GPIOA, 1 for GPIOB, etc.
-    uint16_t         pinNumber;         // Pin number within the port (0-15)
-    Mode             mode;              // Input, Output, Alternate, Analog
-    Pull             pull;              // None, Up, Down
-    uint8_t          alternateFunction; // Alternate function number (0-15),
-                                        // if applicable, 0 otherwise.
+    uint8_t          portIndex;
+    uint16_t         pinNumber; // Pin number within the port (0-15) for SMT32 or (0-32) for ESP32
+    Mode             mode;      // Input, Output, Alternate, Analog
+    Pull             pull;      // None, Up, Down
+    Type             type;
+    Speed            speed;
+    InterruptTrigger interrupt;         // Interrupt trigger type, if applicable
+    uint8_t          alternateFunction; // Alternate function number (0-15), 0 = none.
+    uint8_t          priority;          // NVIC priority (optional)
 };
 
 #endif // _PIN_CONFIG_HPP_
